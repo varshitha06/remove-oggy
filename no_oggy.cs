53fa15db3b16247ac54eb5bf76370165ea2b6b75
varshitha06 <52420416+varshitha06@users.noreply.github.com>
2020-08-13 17:24:29 +0530

    Update no_oggy.cs

--- no_oggy.cs
+++ no_oggy.cs
@@ -5,11 +5,11 @@ class NoOggyChecker
 {
   static void RemoveOggy(List<string> names)
   {
-    foreach (var name in names)
+    for (int i=0; i<names.count;i++)
     {
-        if(name.StartsWith("oggy")) {
-            Console.WriteLine($"Need to remove {name}!");
-            names.Remove(name);
+        if(names[i].StartsWith("oggy")) {
+            Console.WriteLine($"Need to remove {names[i]}!");
+            names.Remove(names[i]);
         }
     }
   }
